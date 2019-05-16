1bc9763d83f8e345b9d7b2d9638b98361348a728
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2019-05-16 19:06:03 +0200

    Add files via upload

---
+++ firmware/Release/Src/stm32l0xx_it.d
@@ -0,0 +1,88 @@
+Src/stm32l0xx_it.o: ../Src/stm32l0xx_it.c \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Inc/stm32l0xx_hal_conf.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Inc/main.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l011xx.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Include/core_cm0plus.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_spi.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h \
+ C:/Users/SP5WWP/Documents/M17_ANL/Inc/stm32l0xx_it.h
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Inc/stm32l0xx_hal_conf.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Inc/main.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l011xx.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Include/core_cm0plus.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_spi.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h:
+
+C:/Users/SP5WWP/Documents/M17_ANL/Inc/stm32l0xx_it.h:
---
+++ firmware/Release/Src/subdir.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/stm32l0xx_hal_msp.c \
+../Src/stm32l0xx_it.c \
+../Src/system_stm32l0xx.c 
+
+OBJS += \
+./Src/main.o \
+./Src/stm32l0xx_hal_msp.o \
+./Src/stm32l0xx_it.o \
+./Src/system_stm32l0xx.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/stm32l0xx_hal_msp.d \
+./Src/stm32l0xx_it.d \
+./Src/system_stm32l0xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m0plus -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32L011xx -I"C:/Users/SP5WWP/Documents/M17_ANL/Inc" -I"C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc" -I"C:/Users/SP5WWP/Documents/M17_ANL/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy" -I"C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Device/ST/STM32L0xx/Include" -I"C:/Users/SP5WWP/Documents/M17_ANL/Drivers/CMSIS/Include"  -O2 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
