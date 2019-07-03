a7812531f55ee77a4c1f5114ecd5b789f5b55248
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2019-07-03 20:58:29 +0200

    Add files via upload

--- firmware/L031/Src/main.c
+++ firmware/L031/Src/main.c
@@ -708,7 +708,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 31999;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 499;
+  htim2.Init.Period = 9;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
   {
