45553643c6284989ada40525d0deee39385b74a8
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2019-09-17 19:08:27 +0200

    Add files via upload

--- firmware/L031/Inc/stm32l0xx_it.h
+++ firmware/L031/Inc/stm32l0xx_it.h
@@ -54,6 +54,7 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void EXTI4_15_IRQHandler(void);
 void TIM2_IRQHandler(void);
+void TIM21_IRQHandler(void);
 void USART2_IRQHandler(void);
 
 #ifdef __cplusplus
